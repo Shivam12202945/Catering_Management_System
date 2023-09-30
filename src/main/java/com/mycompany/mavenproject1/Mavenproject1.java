f6b925c70c51278ae806ed3cb1bd2f1f99c5fda7
Shivam12202945 <137088923+Shivam12202945@users.noreply.github.com>
2023-09-30 15:48:09 +0530

    Add files via upload

---
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,31 @@
+///*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+// */
+//
+///**
+// *
+// * @author kshub
+// */
+//public class Mavenproject1 {
+//    
+//}
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author kshub
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+                 new AdminLogin().show();
+    }
+}
